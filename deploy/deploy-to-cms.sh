0facb2112fdc06d1350db085194221192e4348f5
Daniel <lguo@caltech.edu>
2018-09-24 11:50:08 -0700

    Update permissions

--- deploy/deploy-to-cms.sh
+++ deploy/deploy-to-cms.sh
@@ -35,21 +35,21 @@ echo '[INFO] Uploading index.html...'
 # REDIRECTS contains all relative URLS (w.r.t netlab.caltech.edu) that need to redirect back to the netlab main index.html page.
 # This will ensure the URLs redirect properly under the CMS DNS service
 
-REDIRECTS=("", "research/", "research/power-systems-steady-state/", "research/power-systems-dynamics/", \
-  "research/electric-vehicles/", "research/communication-networks/", "people/", \
-  "publications/", "resources/", "acknowledgement/", "staging/")
+REDIRECTS=("" "research/" "research/power-systems-steady-state/" "research/power-systems-dynamics/" \
+  "research/electric-vehicles/" "research/communication-networks/" "people/" \
+  "publications/" "resources/" "acknowledgement/" "staging/")
 
 for path in "${REDIRECTS[@]}"
 do
-  sshpass -p ${PASSWORD} ssh ${USERNAME}@login.cms.caltech.edu 'mkdir -p /cs/networks/netlab/${path}'
-  sshpass -p ${PASSWORD} scp -r ./dist/index.html ${USERNAME}@login.cms.caltech.edu:/cs/networks/netlab/${path}
+  sshpass -p ${PASSWORD} ssh ${USERNAME}@login.cms.caltech.edu "mkdir -p /cs/networks/netlab/${path}"
+  sshpass -p ${PASSWORD} scp -r ./dist/index.html ${USERNAME}@login.cms.caltech.edu:/cs/networks/netlab/${path}index.html
 done
 
 echo '[INFO] Updating file group and permissions...'
 for path in "${REDIRECTS[@]}"
 do
-  sshpass -p ${PASSWORD} ssh ${USERNAME}@login.cms.caltech.edu 'chgrp networks /cs/networks/netlab/${path}index.html'
-  sshpass -p ${PASSWORD} ssh ${USERNAME}@login.cms.caltech.edu 'chmod -R g+w /cs/networks/netlab/${path}index.html'
+  sshpass -p ${PASSWORD} ssh ${USERNAME}@login.cms.caltech.edu "chgrp networks /cs/networks/netlab/${path}index.html"
+  sshpass -p ${PASSWORD} ssh ${USERNAME}@login.cms.caltech.edu "chmod -R g+w /cs/networks/netlab/${path}index.html"
 done
 
 sshpass -p ${PASSWORD} ssh ${USERNAME}@login.cms.caltech.edu 'chgrp networks /cs/networks/netlab/crossdomain.xml'
